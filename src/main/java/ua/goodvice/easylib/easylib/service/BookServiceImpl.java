007a3f7d0e01202fedbb89501e6edbd515c56a47
goodvice <andriy.lysak.2003@gmail.com>
2023-03-02 19:22:07 +0200

    [add] Added BookServiceImpl.java
    Class contains service methods to ineract with table "book"

--- src/main/java/ua/goodvice/easylib/easylib/service/BookServiceImpl.java
+++ src/main/java/ua/goodvice/easylib/easylib/service/BookServiceImpl.java
@@ -1,2 +1,40 @@
-package ua.goodvice.easylib.easylib.service;public class BookServiceImpl {
+package ua.goodvice.easylib.easylib.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ua.goodvice.easylib.easylib.dao.BookRepository;
+import ua.goodvice.easylib.easylib.entity.Book;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookServiceImpl implements BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Override
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    @Override
+    public void saveBook(Book book) {
+        bookRepository.save(book);
+    }
+
+    @Override
+    public Book getBook(int id) {
+        Book book = null;
+        Optional<Book> optional = bookRepository.findById(id);
+        if (optional.isPresent()) {
+            book = optional.get();
+        }
+        return book;
+    }
+
+    @Override
+    public void deleteBook(int id) {
+        bookRepository.deleteById(id);
+    }
 }
